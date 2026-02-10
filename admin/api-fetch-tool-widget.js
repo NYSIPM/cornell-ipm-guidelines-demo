6f8fb91ac7934659a9f5bb75d8d0e76b25e7d7e1
wparken <92596242+wparken@users.noreply.github.com>
2026-02-10 17:13:59 -0500

    Update api-fetch-tool-widget.js

--- admin/api-fetch-tool-widget.js
+++ admin/api-fetch-tool-widget.js
@@ -15,8 +15,9 @@
   const createClass = window.createClass;
   const h = window.h;
 
+  //https://cropandpestguides.cce.cornell.edu/NewGuidelinesTableImportTest/api/example
   const DEFAULT_ENDPOINT =
-    "https://cropandpestguides.cce.cornell.edu/NewGuidelinesTableImportTest/api/example";
+    "https://cropandpestguides.cce.cornell.edu/GuidelineTable/api/guideline-table/pesticides?guidelineId=3&pestId=208&siteId=29";
 
   function buildApiBlock(endpoint, cached) {
     return (
